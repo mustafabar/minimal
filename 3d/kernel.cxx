29805b7c81d888dd7b6bb33f63f59f888329c930
Mustafa <musbar@gmail.com>
2020-04-12 13:26:11 +0200

    fix/update kernel.cxx

--- 3d/kernel.cxx
+++ 3d/kernel.cxx
@@ -1,87 +1,92 @@
-#include "build_tree.h"
 #include "kernel.h"
-#include "timer.h"
-#if EXAFMM_EAGER
-#include "traverse_eager.h"
-#elif EXAFMM_LAZY
-#include "traverse_lazy.h"
-#endif
 using namespace exafmm;
 
 int main(int argc, char ** argv) {
-  const int numBodies = 100000;                                 // Number of bodies
-  P = 10;                                                       // Order of expansions
-  ncrit = 128;                                                   // Number of bodies per leaf cell
-  theta = 0.4;                                                  // Multipole acceptance criterion
+  P = atoi(argv[1]);
+  initKernel();
 
-  printf("--- %-16s ------------\n", "FMM Profiling");          // Start profiling
-  //! Initialize bodies
-  start("Initialize bodies");                                   // Start timer
-  Bodies bodies(numBodies);                                     // Initialize bodies
-  real_t average = 0;                                           // Average charge
-  srand48(0);                                                   // Set seed for random number generator
-  for (size_t b=0; b<bodies.size(); b++) {                      // Loop over bodies
-    for (int d=0; d<3; d++) {                                   //  Loop over dimension
-      bodies[b].X[d] = drand48() * 2 * M_PI - M_PI;             //   Initialize positions
-    }                                                           //  End loop over dimension
-    bodies[b].q = drand48() - .5;                               //  Initialize charge
-    average += bodies[b].q;                                     //  Accumulate charge
-    bodies[b].p = 0;                                            //  Clear potential
-    for (int d=0; d<3; d++) bodies[b].F[d] = 0;                 //  Clear force
-  }                                                             // End loop over bodies
-  average /= bodies.size();                                     // Average charge
-  for (size_t b=0; b<bodies.size(); b++) {                      // Loop over bodies
-    bodies[b].q -= average;                                     // Charge neutral
-  }                                                             // End loop over bodies
-  stop("Initialize bodies");                                    // Stop timer
+  // P2M
+  Bodies jbodies(1);
+  for (int d=0; d<3; d++) jbodies[0].X[d] = 2;
+  jbodies[0].q = 1;
+  Cells cells(4);
+  Cell * Cj = &cells[0];
+  Cj->X[0] = 3;
+  Cj->X[1] = 1;
+  Cj->X[2] = 1;
+  Cj->R = 1;
+  Cj->BODY = &jbodies[0];
+  Cj->NBODY = jbodies.size();
+  Cj->M.resize(NTERM, 0.0);
+  P2M(Cj);
 
-  //! Build tree
-  start("Build tree");                                          // Start timer
-  Cells cells = buildTree(bodies);                              // Build tree
-  stop("Build tree");                                           // Stop timer
+  // M2M
+  Cell * CJ = &cells[1];
+  CJ->CHILD = Cj;
+  CJ->NCHILD = 1;
+  CJ->X[0] = 4;
+  CJ->X[1] = 0;
+  CJ->X[2] = 0;
+  CJ->R = 2;
+  CJ->M.resize(NTERM, 0.0);
+  M2M(CJ);
 
-  //! FMM evaluation
-  start("P2M & M2M");                                           // Start timer
-  initKernel();                                                 // Initialize kernel
-  upwardPass(cells);                                            // Upward pass for P2M, M2M
-  stop("P2M & M2M");                                            // Stop timer
-  start("M2L & P2P");                                           // Start timer
-  horizontalPass(cells, cells);                                 // Horizontal pass for M2L, P2P
-  stop("M2L & P2P");                                            // Stop timer
-  start("L2L & L2P");                                           // Start timer
-  downwardPass(cells);                                          // Downward pass for L2L, L2P
-  stop("L2L & L2P");                                            // Stop timer
+  // M2L
+  Cell * CI = &cells[2];
+  CI->X[0] = -4;
+  CI->X[1] = 0;
+  CI->X[2] = 0;
+  CI->R = 2;
+  CI->L.resize(NTERM, 0.0);
+  M2L(CI, CJ);
 
-  //! Direct N-Body
-  start("Direct N-Body");                                       // Start timer
-  const int numTargets = 10;                                    // Number of targets for checking answer
-  Bodies jbodies = bodies;                                      // Save bodies in jbodies
-  int stride = bodies.size() / numTargets;                      // Stride of sampling
-  for (int b=0; b<numTargets; b++) {                            // Loop over target samples
-    bodies[b] = bodies[b*stride];                               //  Sample targets
-  }                                                             // End loop over target samples
-  bodies.resize(numTargets);                                    // Resize bodies
-  Bodies bodies2 = bodies;                                      // Backup bodies
-  for (size_t b=0; b<bodies.size(); b++) {                      // Loop over bodies
-    bodies[b].p = 0;                                            //  Clear potential
-    for (int d=0; d<3; d++) bodies[b].F[d] = 0;                 //  Clear force
-  }                                                             // End loop over bodies
-  direct(bodies, jbodies);                                      // Direct N-Body
-  stop("Direct N-Body");                                        // Stop timer
+  // L2L
+  Cell * Ci = &cells[3];
+  CI->CHILD = Ci;
+  CI->NCHILD = 1;
+  Ci->X[0] = -3;
+  Ci->X[1] = 1;
+  Ci->X[2] = 1;
+  Ci->R = 1;
+  Ci->L.resize(NTERM, 0.0);
+  L2L(CI);
 
-  //! Verify result
+  // L2P
+  Bodies bodies(1);
+  bodies[0].X[0] = -2;
+  bodies[0].X[1] = 2;
+  bodies[0].X[2] = 2;
+  bodies[0].q = 1;
+  bodies[0].p = 0;
+  for (int d=0; d<3; d++) bodies[0].F[d] = 0;
+  Ci->BODY = &bodies[0];
+  Ci->NBODY = bodies.size();
+  L2P(Ci);
+
+  // P2P
+  Bodies bodies2(1);
+  for (size_t b=0; b<bodies2.size(); b++) {
+    bodies2[b] = bodies[b];
+    bodies2[b].p = 0;
+    for (int d=0; d<3; d++) bodies2[b].F[d] = 0;
+  }
+  Cj->NBODY = jbodies.size();
+  Ci->NBODY = bodies2.size();
+  Ci->BODY = &bodies2[0];
+  P2P(Ci, Cj);
+
+  // Verify results
   real_t pDif = 0, pNrm = 0, FDif = 0, FNrm = 0;
-  for (size_t b=0; b<bodies.size(); b++) {                      // Loop over bodies & bodies2
-    pDif += (bodies[b].p - bodies2[b].p) * (bodies[b].p - bodies2[b].p);// Difference of potential
-    pNrm += bodies2[b].p * bodies2[b].p;                        //  Value of potential
-    FDif += (bodies[b].F[0] - bodies2[b].F[0]) * (bodies[b].F[0] - bodies2[b].F[0]) +// Difference of force
-      (bodies[b].F[1] - bodies2[b].F[1]) * (bodies[b].F[1] - bodies2[b].F[1]) +// Difference of force
-      (bodies[b].F[2] - bodies2[b].F[2]) * (bodies[b].F[2] - bodies2[b].F[2]);// Difference of force
-    FNrm += bodies[b].F[0] * bodies[b].F[0] + bodies[b].F[1] * bodies[b].F[1] +// Value of force
+  for (size_t b=0; b<bodies.size(); b++) {
+    pDif += (bodies[b].p - bodies2[b].p) * (bodies[b].p - bodies2[b].p);
+    pNrm += bodies[b].p * bodies[b].p;
+    FDif += (bodies[b].F[0] - bodies2[b].F[0]) * (bodies[b].F[0] - bodies2[b].F[0]) +
+      (bodies[b].F[1] - bodies2[b].F[1]) * (bodies[b].F[1] - bodies2[b].F[1]) +
+      (bodies[b].F[2] - bodies2[b].F[2]) * (bodies[b].F[2] - bodies2[b].F[2]);
+    FNrm += bodies[b].F[0] * bodies[b].F[0] + bodies[b].F[1] * bodies[b].F[1] +
       bodies[b].F[2] * bodies[b].F[2];
-  }                                                             // End loop over bodies & bodies2
-  printf("--- %-16s ------------\n", "FMM vs. direct");         // Print message
-  printf("%-20s : %8.5e s\n","Rel. L2 Error (p)", sqrt(pDif/pNrm));// Print potential error
-  printf("%-20s : %8.5e s\n","Rel. L2 Error (F)", sqrt(FDif/FNrm));// Print force error
+  }
+  printf("%-20s : %8.5e s\n","Rel. L2 Error (p)", sqrt(pDif/pNrm));
+  printf("%-20s : %8.5e s\n","Rel. L2 Error (F)", sqrt(FDif/FNrm));
   return 0;
 }
